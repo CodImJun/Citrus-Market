57c0500e49dcc8ba4c7f5c09b4c2c3167170102e
CodImJun <codimjun@gmail.com>
2024-02-15 01:45:55 +0900

    fix: fix bugs not applicable to tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,14 +5,15 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
         primary: "#f26e22",
         grey: {
-          100: "c4c4c4",
-          200: "767676",
+          100: "#c4c4c4",
+          200: "#767676",
         },
         black: "#000000",
         white: "#ffffff",
