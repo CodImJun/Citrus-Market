ba3ec4d3f86e345f72dc9ef1f44f5202e7a37bb6
CodImJun <codimjun@gmail.com>
2024-02-15 01:05:20 +0900

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: "#f26e22",
+        grey: {
+          100: "c4c4c4",
+          200: "767676",
+        },
+        black: "#000000",
+        white: "#ffffff",
       },
     },
   },
