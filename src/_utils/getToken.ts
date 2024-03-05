f75001ca47e126e880ba5e45c9334a09aa4d5a5f
CodImJun <codimjun@gmail.com>
2024-03-05 18:01:47 +0900

    remove: remove unnecessary code

--- src/_utils/getToken.ts
+++
@@ -1,6 +0,0 @@
-import { ACCESS_TOKEN_KEY, REFRESH_TOKEN_KEY } from "@/_constants";
-
-export const getToken = {
-  accessToken: () => localStorage.getItem(ACCESS_TOKEN_KEY),
-  refreshToken: () => localStorage.getItem(REFRESH_TOKEN_KEY),
-};
