a81c91484fc73264f0dc81f1cf93a4cb37c2b2c8
CodImJun <codimjun@gmail.com>
2024-03-18 16:04:37 +0900

    feat: add Query Provider

---
+++ src/app/provider.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import React from "react";
+
+const getQueryClient = React.cache(() => new QueryClient());
+
+export const Provider = ({ children }: React.PropsWithChildren) => {
+  const queryClient = getQueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
