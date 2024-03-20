cb554e7f2fb6b31a7f9874301a000295d9b75890
CodImJun <codimjun@gmail.com>
2024-03-20 22:39:11 +0900

    feat: add main page - API

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,24 +1,17 @@
 "use client";
 
-import { Button } from "@/_components";
-import { instance } from "@/_states/server";
+import { Button, PostList } from "@/_components";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
-import { FeedList } from "./_components";
-import { ACCESS_TOKEN_KEY, REFRESH_TOKEN_KEY } from "@/_constants";
+import { useGetFollowingPostList } from "./_states/client/useGetFollowingPostList";
+import Link from "next/link";
 
 const MainPage = () => {
-  const route = useRouter();
-  // TODO: Add Profile API & Change Client State
-  const [isFollow, setIsFollow] = useState(false);
-
-  const handleRouteSearchPage = () => route.push("/search");
+  const { data: followingPostList } = useGetFollowingPostList();
 
   return (
     <>
-      {isFollow ? (
-        <>
+      {followingPostList ? (
+        <div className="flex flex-col items-center justify-center gap-y-[2rem] mt-[22rem]">
           <Image
             src="/symbol-logo-gray.png"
             alt="logo image"
@@ -29,52 +22,14 @@ const MainPage = () => {
           <div className="text-14-400-14 text-grey-700">
             유저를 검색해 팔로우 해보세요!
           </div>
-          <Button
-            type="button"
-            size="L"
-            w="w-[12rem]"
-            onClick={handleRouteSearchPage}
-          >
-            검색하기
-          </Button>
-          <button
-            type="button"
-            onClick={async () => {
-              const res = await instance.post("/user", {
-                user: {
-                  email: "limtest1234@naver.com",
-                  password: "limtest1234",
-                  username: "limtester",
-                  accountname: "limtester",
-                },
-              });
-              console.log(res.data);
-            }}
-          >
-            회원가입
-          </button>
-          <button
-            type="button"
-            onClick={async () => {
-              const res = await instance.post("/user/login", {
-                user: {
-                  email: "limtest1234@naver.com",
-                  password: "limtest1234",
-                },
-              });
-              console.log(res.data);
-              localStorage.setItem(ACCESS_TOKEN_KEY, res.data.user.token);
-              localStorage.setItem(
-                REFRESH_TOKEN_KEY,
-                res.data.user.refreshToken
-              );
-            }}
-          >
-            로그인
-          </button>
-        </>
+          <Link href="/search">
+            <Button type="button" size="L" w="w-[12rem]">
+              검색하기
+            </Button>
+          </Link>
+        </div>
       ) : (
-        <FeedList />
+        <PostList list={followingPostList ?? []} />
       )}
     </>
   );
