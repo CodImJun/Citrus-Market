d12a90f0db0c39e8c39e6e61c77f41b7bfe7fe6d
CodImJun <codimjun@gmail.com>
2024-03-22 01:39:48 +0900

    feat: add useSearchUser API - client query

---
+++ src/app/search/_states/client/useSearchUser.ts
@@ -0,0 +1,13 @@
+import { SearchAPI } from "@/_apis";
+import { queryKeys } from "@/_states";
+import { isEmptyString } from "@/_utils";
+import { useQuery } from "@tanstack/react-query";
+
+export const useSearchUser = (keyword: string) => {
+  return useQuery({
+    queryKey: [queryKeys.search, keyword],
+    queryFn: () => SearchAPI.searchUser({ keyword }),
+    enabled: !isEmptyString(keyword),
+    staleTime: 6 * 10 * 1000,
+  });
+};
