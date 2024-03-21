9d344167ebd3323d1bf832d5aa5c6a943040334a
CodImJun <codimjun@gmail.com>
2024-03-22 01:40:22 +0900

    feat: add usePagination hook

---
+++ src/_hooks/usePagination.ts
@@ -0,0 +1,36 @@
+import { UseQueryResult } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
+
+type UsePaginationProps<T> = {
+  data: T[];
+  isSuccess: UseQueryResult["isSuccess"];
+  itemsPerPage: number;
+};
+
+export const usePagination = <T>({
+  data,
+  isSuccess,
+  itemsPerPage,
+}: UsePaginationProps<T>) => {
+  const [paginationedList, setPaginationedList] = useState<T[]>([]);
+  const [currentPage, setCurrentPage] = useState(0);
+
+  useEffect(() => {
+    isSuccess && setPaginationedList(data.slice(0, itemsPerPage));
+  }, [data, isSuccess, itemsPerPage]);
+
+  const handleLoadMore = () => {
+    const nextPage = currentPage + 1;
+
+    setCurrentPage(nextPage);
+    setPaginationedList((prevList) => [
+      ...prevList,
+      ...data.slice(
+        nextPage * itemsPerPage,
+        nextPage * itemsPerPage + itemsPerPage
+      ),
+    ]);
+  };
+
+  return { paginationedList, handleLoadMore };
+};
