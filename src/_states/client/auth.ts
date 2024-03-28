08c6a3a673bcd0b79c546c31dcb50af0d3b4cfb7
CodImJun <codimjun@gmail.com>
2024-03-28 18:54:35 +0900

    feat: add initialState in auth store

--- src/_states/client/auth.ts
+++ src/_states/client/auth.ts
@@ -1,5 +1,8 @@
+"use client";
+
 import { LoginResponse } from "@/_apis";
 import { ACCESS_TOKEN_KEY, REFRESH_TOKEN_KEY } from "@/_constants";
+import { UserType } from "@/_types";
 import { isStatusInData } from "@/_utils";
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
@@ -7,7 +10,7 @@ import { immer } from "zustand/middleware/immer";
 
 type UseAuthStoreState = {
   isLoggedIn: boolean;
-  loginID: string;
+  loginInfo: Pick<UserType, "_id" | "accountname" | "username">;
 };
 
 type UseAuthStoreAction = {
@@ -15,28 +18,38 @@ type UseAuthStoreAction = {
   handleLogOut: () => void;
 };
 
+const initialState = {
+  isLoggedIn: Boolean(localStorage.getItem(ACCESS_TOKEN_KEY)),
+  loginInfo: {
+    _id: "",
+    accountname: "",
+    username: "",
+  },
+};
+
 export const useAuthStore = create<UseAuthStoreState & UseAuthStoreAction>()(
   persist(
     immer((set) => ({
-      isLoggedIn: Boolean(localStorage.getItem(ACCESS_TOKEN_KEY)),
-      loginID: "",
+      ...initialState,
       handleSetLoginInfo: (data) => {
         set((state) => {
           if (!isStatusInData(data)) {
             state.isLoggedIn = true;
-            state.loginID = data.user._id;
+            state.loginInfo = {
+              _id: data.user._id,
+              accountname: data.user.accountname,
+              username: data.user.username,
+            };
             localStorage.setItem(ACCESS_TOKEN_KEY, data.user.token);
             localStorage.setItem(REFRESH_TOKEN_KEY, data.user.refreshToken);
           }
         });
       },
-      handleLogOut: () =>
-        set((state) => {
-          state.isLoggedIn = false;
-          state.loginID = "";
-          localStorage.removeItem(ACCESS_TOKEN_KEY);
-          localStorage.removeItem(REFRESH_TOKEN_KEY);
-        }),
+      handleLogOut: () => {
+        set(initialState);
+        localStorage.removeItem(ACCESS_TOKEN_KEY);
+        localStorage.removeItem(REFRESH_TOKEN_KEY);
+      },
     })),
     {
       name: "CITRUS_AUTH_STORE",
