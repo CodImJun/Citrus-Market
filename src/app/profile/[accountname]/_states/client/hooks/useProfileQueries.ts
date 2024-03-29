500ff5bdf8745384cbce0f0a33c8f6aede689eb8
CodImJun <codimjun@gmail.com>
2024-03-29 14:35:27 +0900

    fix: fix fetching bugs

--- src/app/profile/[accountname]/_states/client/hooks/useProfileQueries.ts
+++ src/app/profile/[accountname]/_states/client/hooks/useProfileQueries.ts
@@ -6,15 +6,12 @@ export const useProfileQueries = (accountname: string) => {
   const getProfileQueryOptions = {
     queryKey: [queryKeys.profile, accountname],
     queryFn: () => ProfileAPI.getPersonalProfile({ accountname }),
-    staleTime: 6 * 10 * 1000,
-    refetchOnWindowFocus: true,
   };
 
   const getProductListQueryOptions = {
     queryKey: [queryKeys.product, accountname],
     queryFn: () => ProductAPI.getProductList({ accountname }),
   };
-
   const getPostListQueryOptions = {
     queryKey: [queryKeys.post, accountname],
     queryFn: () => PostAPI.getMyPostList({ accountname }),
--- src/app/profile/[accountname]/page.tsx
+++ src/app/profile/[accountname]/page.tsx
@@ -11,26 +11,31 @@ import { ProfilePageProps } from "./page.types";
 const ProfilePage = ({ params }: ProfilePageProps) => {
   const queryResults = useProfileQueries(params.accountname);
   const [getProfileQuery, getProductQuery, getPostQuery] = queryResults;
-  getProfileQuery.refetch;
 
-  if (queryResults.some((query) => query.isLoading)) return null;
-  if (queryResults.some((query) => query.isError)) return null;
-
-  if (!getProfileQuery.data) return null;
-  if (!getProductQuery.data) return null;
-  if (!getPostQuery.data) return null;
-
-  return (
-    <>
-      <UserProfileSection
-        profileInfo={getProfileQuery.data}
-        params={params}
-        refetch={getProfileQuery.refetch}
-      />
-      <UserProductSection productList={getProductQuery.data} />
-      <UserPostSection postList={getPostQuery.data} />
-    </>
+  const isLoading = queryResults.some((query) => query.isLoading);
+  const isError = queryResults.some((query) => query.isError);
+  const isFetching = queryResults.some((query) => query.isFetching);
+  const isData = !(
+    !getProfileQuery.data ||
+    !getProductQuery.data ||
+    !getPostQuery.data
   );
+
+  if (isLoading) return null;
+  if (isError) return null;
+  if (isFetching) return null;
+  if (isData)
+    return (
+      <>
+        <UserProfileSection
+          profileInfo={getProfileQuery.data}
+          params={params}
+          refetch={getProfileQuery.refetch}
+        />
+        <UserProductSection productList={getProductQuery.data} />
+        <UserPostSection postList={getPostQuery.data} />
+      </>
+    );
 };
 
 export default ProfilePage;
